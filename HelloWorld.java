b2b4f4f6bb2384a5ac8bca97efedf77684bf8d86
rk-prog <57847150+rk-prog@users.noreply.github.com>
2019-11-16 17:19:06 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.practicals;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        for (int i=0;i<10;i++){
+            System.out.println("hello world!! " + i);
+        }
+    }
+}
